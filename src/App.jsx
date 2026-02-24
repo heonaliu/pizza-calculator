4fd5d8a2bc982459e9b6a5c6e34d0f58f4bfa2c6
Heona <heonaliu@gmail.com>
2026-02-24 18:54:56 -0500

    add a big pizza icon at the top

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import icon from "./assets/images/iconLogo.png";
 import { PartyCalculator } from "./components/PartyCalculator";
 function App() {
   return (
     <div className="app">
       <div>
+        <img className="pizza-header" src={icon} alt="pizza" />
         <h1 className="header">Party Pizza Calculator</h1>
       </div>
       <PartyCalculator />
--- src/components/ResultsSection.jsx
+++ src/components/ResultsSection.jsx
@@ -1,7 +1,12 @@
+import pizzaIcon from "../assets/images/pizzaIcon.png";
+
 export const ResultsSection = ({ totalSlices, pizzaNeed }) => {
   return (
     <div className="results-section">
-      <p>Total Slices: {totalSlices}</p>
+      <p>
+        Total Slices: {totalSlices}{" "}
+        <img src={pizzaIcon} className="pizza-icon" alt="pizza" />
+      </p>
       <p>Pizzas Needed: {pizzaNeed}</p>
     </div>
   );
